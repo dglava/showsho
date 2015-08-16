cfd532db7a0239d24157d62ff75ce354a58c8e08
dglava <dinomol@mail.com>
2015-08-16 15:37:29 +0200

    Added LICENSE and README; Fixed downloadShows()
    downloadShows() checks now if the show has ended, to prevent downloading
    the last episode even if the show has ended weeks ago.

--- showsho/showsho.py
+++ showsho/showsho.py
@@ -1,3 +1,19 @@
+# Showsho
+# Copyright (C) 2015  Dino Duratović <dinomol@mail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import datetime
 import json
 
@@ -107,7 +123,7 @@ def downloadShows(shows):
     no_shows_to_download = True
 
     for show in shows:
-        if show.new_episode:
+        if show.new_episode and not show.ended:
             no_shows_to_download = False
 
             torrents = utils.getTorrents(show)
